2a7afcd5b5704ac78201456f2e633f38696ff81d
JK <jk.4work@gmail.com>
2023-12-25 22:56:36 +0200

    set on ds

--- src/com/SirmaAcademy/SharedProjectsFinder/DataStorage.java
+++ src/com/SirmaAcademy/SharedProjectsFinder/DataStorage.java
@@ -3,43 +3,26 @@ package com.SirmaAcademy.SharedProjectsFinder;
 import com.SirmaAcademy.SharedProjectsFinder.Data.Pair;
 import com.SirmaAcademy.SharedProjectsFinder.Data.WorkingTimes;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.TreeSet;
 
 public class DataStorage {
-    private static ArrayList<WorkingTimes> workingTimes;
+    private static HashSet<WorkingTimes> workingTimes;
     private static TreeSet<Integer> employees; // ID's of all employees
     private static HashMap<Double, Pair> pairs;
 
-    public static ArrayList<WorkingTimes> getWorkingTimes() {
+    public static HashSet<WorkingTimes> getWorkingTimes() {
         return workingTimes;
-    }
-
-    public static void setWorkingTimes(ArrayList<WorkingTimes> workingTimes) {
-        DataStorage.workingTimes = workingTimes;
     }
 
     public static TreeSet<Integer> getEmployees() {
         return employees;
     }
 
-    public static void setEmployees(TreeSet<Integer> employees) {
-        DataStorage.employees = employees;
-    }
 
     public static HashMap<Double, Pair> getPairs() {
         return pairs;
-    }
-
-    public static void setPairs(HashMap<Double, Pair> pairs) {
-        DataStorage.pairs = pairs;
-    }
-
-    public DataStorage() {
-        employees = new TreeSet<>();
-        pairs = new HashMap<>();
-        workingTimes = new ArrayList<>();
     }
 
     public static void addEmployee(int id) {
