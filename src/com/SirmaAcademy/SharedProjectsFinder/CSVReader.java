74817387bbeca4b3ab5a520410623b8ebfbf3819
JK <jk.4work@gmail.com>
2023-12-25 15:13:59 +0200

    edited reader

--- src/com/SirmaAcademy/SharedProjectsFinder/CSVReader.java
+++ src/com/SirmaAcademy/SharedProjectsFinder/CSVReader.java
@@ -1,5 +1,21 @@
 package com.SirmaAcademy.SharedProjectsFinder;
 
-public class CSVReader {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-}
+public class CSVReader implements Reader {
+
+
+  public void read(String path){
+      try (
+              Scanner scanner = new Scanner(new File(path))) {
+
+        while (scanner.hasNextLine()) {
+        }
+      } catch (
+              FileNotFoundException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
--- src/com/SirmaAcademy/SharedProjectsFinder/Reader.java
+++ src/com/SirmaAcademy/SharedProjectsFinder/Reader.java
@@ -1,4 +1,7 @@
 package com.SirmaAcademy.SharedProjectsFinder;
 
 public interface Reader {
+
+    public void read(String path);
+
 }
